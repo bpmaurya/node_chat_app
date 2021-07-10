e750eece9e7a63faebed5f323de5d13e3dd681ae
bpmaurya <realvedprakash@gmail.com>
2021-07-10 19:05:43 +0530

    add timestamp

---
+++ playground/time.js
@@ -0,0 +1,7 @@
+const moment = require('moment')
+
+var date = moment();
+
+
+var someTimeStamp = moment().valueOf();
+
--- public/js/index.js
+++ public/js/index.js
@@ -15,17 +15,18 @@ socket.on('newEmail',function(email) {
 
 socket.on('newMessage',function(message) {
     console.log("new message",message);
-
+    var formattedTime = moment(message.createdAt).format('h:mm a')
     var li = jQuery('<li></li>')
-    li.text(`${message.from}:${message.text}`)
+    li.text(` ${message.from} ${formattedTime}:${message.text}`)
     jQuery('#messages').append(li)
 })
 
 socket.on('newLocationMessage', function(message) {
+ var formattedTime = moment(message.createdAt).format('h:mm a')
  var li = jQuery('<li></li>');
  var a = jQuery('<a target="_blank">My current location</a>');
 
- li.text(`${message.from}:`)
+ li.text(`${message.from} ${formattedTime}:`)
  a.attr('href', message.url);
  li.append(a);
  jQuery('#messages').append(li)
